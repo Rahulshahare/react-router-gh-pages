04c6273f515a4ad34f775f011e3848cdc83376d1
Rahulshahare <rahulshahare@gmail.com>
2025-05-17 10:26:37 +0530

    404 component

--- src/App.js
+++ src/App.js
@@ -4,17 +4,19 @@ import Navbar from './components/Navbar';
 import Home from './components/Home';
 import About from './components/About';
 import Contact from './components/Contact';
+import NotFound from './components/404';
 
 function App() {
   return (
     <Router basename='/react-router-gh-pages'>
-    {/* <Router > for local */}
+    {/* <Router> for local */}
       <div style={{ textAlign: 'center' }}>
         <Navbar/>
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/about" element={<About />} />
           <Route path="/contact" element={<Contact />} />
+          <Route path="*" element={<NotFound />} />
         </Routes>
       </div>
     </Router>
---
+++ src/components/404.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function _404 (){
+    return (
+        <div>
+            <h1>404</h1>
+            <h2>Page Not Found</h2>
+        </div>
+    )
+}
+export default _404;
